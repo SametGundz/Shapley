0db600a5bd22e911329204dd16514ae008c400b8
Samet Gündüz <46328316+SametGundz@users.noreply.github.com>
2019-03-22 22:05:44 +0300

    Add files via upload

---
+++ taslak/js/main.js
@@ -0,0 +1,18 @@
+function norwayN() {
+	document.getElementById("norwayN").style.visibility="visible";
+}
+function norwayO() {
+	document.getElementById("norwayO").style.visibility="visible";
+}
+function norwayR() {
+	document.getElementById("norwayR").style.visibility="visible";
+}
+function norwayW() {
+	document.getElementById("norwayW").style.visibility="visible";
+}
+function norwayA() {
+	document.getElementById("norwayA").style.visibility="visible";
+}
+function norwayY() {
+	document.getElementById("norwayY").style.visibility="visible";
+}
